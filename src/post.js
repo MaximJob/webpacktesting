7daf63e065e85ae649f57ab9964af7761db32fb0
Yoku <m.malev@vpene.ru>
2021-10-12 12:29:10 +0300

    e

--- src/post.js
+++ src/post.js
@@ -5,6 +5,10 @@ export default class Post {
         this.date = new Date()
     }
 
+    /*
+    WOWWOWOWOWOWOOWO
+     */
+
     toString() {
         return JSON.stringify({
             title: this.title,
